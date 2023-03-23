efdf10a1cce3ac01c39b8b3ca91b3a074be84bc8
exgael <benoitbarbier.dev@gmail.com>
2023-03-23 00:09:16 +0000

    test slipted into multiple ctest

--- tests/test_agent.sh
+++ tests/test_agent.sh
@@ -1,41 +1,26 @@
 #!/bin/bash
 
-num_tests=4
-passed_tests=0
-failed_tests=0
+test_num=$1
+log_file="test_${test_num}.log"
 
-for i in $(seq 1 $num_tests); do
-    echo "Running test $i"
-    log_file="test_${i}.log"
-    # Run the program and save the output to test.log, save the exit status in a variable
-    (../src/codeEnv/codeEnv -X ../src/Agent/Agent -O ../src/BaseAgent/BaseAgent --noGUI > $log_file 2>&1) >/dev/null 2>&1
+echo "Running test $test_num"
 
+# Run the program and save the output to test.log, save the exit status in a variable
+(../src/codeEnv/codeEnv -X ../src/Agent/Agent -O ../src/BaseAgent/BaseAgent --noGUI > $log_file 2>&1) >/dev/null 2>&1
 
-    exit_status=$?
+exit_status=$?
 
-    # Check the exit status
-    if [ $exit_status -eq 0 ]; then
-        echo "The program ran successfully." >> $log_file
-    else
-        echo "There was an error while running the program. Exit status: $exit_status" >> $log_file
-    fi
-
-    # Check if the word "success" is present in test.log
-    if grep -q "success" $log_file; then
-        echo "Test $i passed."
-        passed_tests=$((passed_tests + 1))
-    else
-        echo "Test $i failed."
-        failed_tests=$((failed_tests + 1))
-    fi
-done
-
-echo "Passed tests: $passed_tests"
-echo "Failed tests: $failed_tests"
-
-if [ $failed_tests -eq 0 ]; then
-    echo "All tests passed. The program is stable."
+# Check the exit status
+if [ $exit_status -eq 0 ]; then
+    echo "The program ran successfully." >> $log_file
 else
-    echo "Some tests failed. The program is unstable."
+    echo "There was an error while running the program. Exit status: $exit_status" >> $log_file
+fi
+
+# Check if the word "success" is present in test.log
+if grep -q "success" $log_file; then
+    echo "Test $test_num passed."
+else
+    echo "Test $test_num failed."
     exit 1
 fi
